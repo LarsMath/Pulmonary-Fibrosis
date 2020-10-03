5554f8de77f0bde954c1e2bc580ae1e21d8f6b61
LarsMath <46840219+LarsMath@users.noreply.github.com>
2020-10-03 12:40:28 +0200

    updatesalles

--- PFutils.py
+++ PFutils.py
@@ -1,9 +1,15 @@
-# %% [code]
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 import tensorflow.keras.backend as K
 import keras
+import pathlib
+import pydicom
+
+from sklearn.cluster import KMeans
+from skimage import morphology
+from skimage import measure
+
 
 def get_test_data(files, input_normalization):
     test = pd.read_csv(files)
@@ -56,59 +62,50 @@ def get_test_data(files, input_normalization):
     
     return test_data, submission
 
-def get_train_data(files, pseudo_test_patients, input_normalization, train_on_backward_weeks, negative_normalization = False):
+def get_train_data(files, pseudo_test_patients, train_on_backward_weeks, apply_lungmask, dim):
     df = pd.read_csv(files)
-    patients = []
+    
+    train = pd.DataFrame()
+    images = []
+    index = 0
     for patient in df.Patient.unique()[pseudo_test_patients:]:
-        weekcombinations = []
+        if patient == "ID00011637202177653955184":
+            continue
         weeks = df.loc[df.Patient == patient]['Weeks']
-        for week1 in weeks:
-            dfnew = df.loc[(df.Patient == patient) & (df.Weeks != week1) & ((df.Weeks<week1) | (train_on_backward_weeks))]
-            dfnew = dfnew.assign(Weekdiff_target = week1)
-            dfnew = dfnew.assign(TargetFVC = df.loc[(df.Patient == patient)&(df.Weeks == week1)]['FVC'].values[0])
-            weekcombinations.append(dfnew)
-        patients.append(pd.concat(weekcombinations))
-
-    train = pd.DataFrame(pd.concat(patients))    
+        for weektarget in weeks:
+            dftemp = df.loc[(df.Patient == patient) & (df.Weeks != weektarget) & ((df.Weeks<weektarget) | (train_on_backward_weeks))]
+            dftemp = dftemp.assign(WeekTarget = weektarget)
+            dftemp = dftemp.assign(TargetFVC = df.loc[(df.Patient == patient)&(df.Weeks == weektarget)]['FVC'].values[0])
+            dftemp = dftemp.assign(PatientIndex = index)
+            train = train.append(dftemp, ignore_index = True)
+        index += 1        
+        
+        img_nmbrs = [np.int(file.split(".")[0]) for file in listdir("../input/osic-pulmonary-fibrosis-progression/train/" + patient)]
+        img_nmbrs = np.sort(img_nmbrs)
+        middle = len(img_nmbrs)//2
+        img_nmbrs = img_nmbrs[middle-1:middle+2]
+        center_images = ([pydicom.dcmread("../input/osic-pulmonary-fibrosis-progression/train/" + patient + "/" 
+                                    + str(index) + ".dcm").pixel_array for index in img_nmbrs])
+        if apply_lungmask:
+             center_images = [make_lungmask(image) for image in center_images]
+        center_images = np.array([cv2.resize(image, dsize = (dim,dim)) for image in center_images])
+        center_images = np.moveaxis(center_images, 0, -1)
+        images.append(center_images)
+        
+    train = train.rename(columns={"Weeks":"WeekInit"})
     train["Sex"] = (train['Sex']=="Male").astype(int)
     train = pd.concat([train,pd.get_dummies(train['SmokingStatus'])],axis = 1).reset_index(drop = True)
-    train = train.drop(columns=["SmokingStatus"])
+    train["WeekDiff"] = train["WeekTarget"] - train["WeekInit"]
         
-    for i in range(len(train)):
-        train.loc[i, "Weekdiff_target"] = train.loc[i, "Weekdiff_target"] - train.loc[i, "Weeks"]
+    images = np.array(images)
     
-    non_normalized_FVC = train["FVC"]
-    non_normalized_Weekdiff = train["Weekdiff_target"]
-    non_normalized_FVC = non_normalized_FVC.astype("float32")
-    non_normalized_Weekdiff = non_normalized_Weekdiff.astype("float32")
-    
-    labels = pd.DataFrame(train[["TargetFVC","Weekdiff_target", "FVC"]])
+    labels = pd.DataFrame(train[["TargetFVC"]])
     labels = labels.astype("float32")
-        
-    if input_normalization:
-        train["Age"] = (train["Age"]-50)/50
-        train["Percent"] = train["Percent"]/100
-        train["Weeks"] = train["Weeks"]/100
-        train["Weekdiff_target"] = train["Weekdiff_target"]/100
-        train["FVC"] = train["FVC"]/5000 
     
-    if negative_normalization:
-        train["Age"] = train["Age"]*2-1
-        train["Percent"] = train["Percent"]*2-1
-        train["Weeks"] = train["Weeks"]*2-1
-        train["Weekdiff_target"] = train["Weekdiff_target"]*2-1
-        train["FVC"] = train["FVC"]*2-1
-    
-    train = train[["Weeks", "FVC", "Percent", "Age", "Sex", "Currently smokes",
-                   "Ex-smoker", "Never smoked", "Weekdiff_target", "Patient"]]
+    train = train[["WeekInit", "WeekTarget", "WeekDiff", "FVC", "Percent",
+                   "Age", "Sex", "Currently smokes", "Ex-smoker", "Never smoked", "PatientIndex"]]
 
-    data = {"input_features": train[["Weeks", "FVC", "Percent", "Age", "Sex", 
-                                     "Currently smokes", "Ex-smoker", "Never smoked", "Weekdiff_target"]],
-            "slope_FVC": non_normalized_FVC, "slope_Weekdiff": non_normalized_Weekdiff}
-    
-    
-    
-    return train, data, labels
+    return train, images, labels
 
 def get_pseudo_test_data(files, pseudo_test_patients, input_normalization, random_seed = 42):
     np.random.seed(random_seed)
@@ -192,15 +189,15 @@ def get_fold_indices(folds, train):
     
     fold_pos = [0]
     count = 0
-    for i in np.unique(train["Patient"]):
+    for i in np.unique(train["PatientIndex"]):
         count += 1
-        if count >= (len(fold_pos)*len(np.unique(train.Patient))/folds):
-            fold_pos.append(np.max(np.where(train["Patient"] == i))+1)
+        if count >= (len(fold_pos)*len(np.unique(train.PatientIndex))/folds):
+            fold_pos.append(np.max(np.where(train["PatientIndex"] == i))+1)
             
     return fold_pos
 
 class DataGenerator(keras.utils.Sequence):
-    def __init__(self, list_IDs, config, validation = False, number_of_labels = 3,
+    def __init__(self, list_IDs, config, validation = False, number_of_labels = 1,
                  batch_size = 128, shuffle = True):
         self.number_features = int(config["NUMBER_FEATURES"])
         self.validation = validation
@@ -210,10 +207,14 @@ class DataGenerator(keras.utils.Sequence):
         self.shuffle = shuffle
         self.on_epoch_end()
         self.label_size = number_of_labels
-        self.normalized = config["INPUT_NORMALIZATION"]
+        self.input_normalization = config["INPUT_NORMALIZATION"]
+        self.negative_normalization = config["NEGATIVE_NORMALIZATION"]
         self.correlated = config["GAUSSIAN_NOISE_FVC_CORRELATED"]
         self.percentcorrelated = config["ADD_NOISE_FVC_TO_PERCENT"]
-        if validation:
+        self.dim = config["DIM"]
+        self.use_images = config["USE_IMAGES"]
+        self.predict_slope = config["PREDICT_SLOPE"]
+        if validation and not self.use_images:
             self.batch_size = len(self.list_IDs)
     
     def __len__(self):
@@ -237,36 +238,116 @@ class DataGenerator(keras.utils.Sequence):
     def __data_generation(self, list_IDs_temp):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         # Initialization
-        X = np.empty((self.batch_size, self.number_features))
-        y = np.empty((self.batch_size, self.label_size), dtype=int)
+        X = np.zeros((self.batch_size, self.number_features))
+        y = np.zeros((self.batch_size, self.label_size))
         
-        data = np.load("./train_data.npy", allow_pickle = True)
+        inpdata = np.load("./train_data.npy", allow_pickle = True)
         lab = np.load("./train_labels.npy", allow_pickle = True)
         
         for i, ID in enumerate(list_IDs_temp):
-            X[i,] = np.asarray(data[ID], dtype = "float32")
+            X[i,] = np.asarray(inpdata[ID][0:-1], dtype = "float32")
             y[i,] = np.asarray(lab[ID], dtype = "float32")
-        y = np.asarray(y,dtype = "float32")
+        
+        if self.predict_slope:
+            WeekDiff = X[:,2]
+            InitFVC = X[:,3]
         
         if not self.validation:
-            gaussian_noise = np.empty((self.batch_size, self.number_features))
+            gaussian_noise = np.zeros((self.batch_size, self.number_features))
             for i, sigma in enumerate(self.noise_SDs):
                 gaussian_noise[:,i] = np.random.normal(0, sigma, size = self.batch_size)
-
-            if self.correlated:
-                gauss_y = gaussian_noise[:,1].copy()
-            else:
-                gauss_y = np.random.normal(0, self.noise_SDs[1], size = self.batch_size)
-            if self.normalized:
-                gaussian_noise[:,1] = gaussian_noise[:,1]/5000
-
+                
             X += gaussian_noise.astype("float32")
+            
+            # Index 3 represents FVC, index 4 represents Percent
             if self.percentcorrelated:
-                X[:,2] += gaussian_noise[:,1].astype("float32")*X[:,2]/X[:,1]
-            y[:,2] += gaussian_noise[:,1].astype("float32")
-            y[:,0] += gauss_y.astype("float32")
+                X[:,4] += gaussian_noise[:,3].astype("float32")*X[:,4]/X[:,3]
+            
+            if self.correlated:
+                gauss_y = gaussian_noise[:,3].copy()
+            else:
+                gauss_y = np.random.normal(0, self.noise_SDs[3], size = self.batch_size)
+                
+            y[:,0] += gauss_y
+            
+        if self.input_normalization:
+            X[:,0:2] = X[:,0:2]/100 #Weeks
+            X[:,3] = X[:,3]/5000    #FVC
+            X[:,4] = X[:,4]/100     #Percent
+            X[:,5] = (X[:,5]-50)/50 #Age
+                
+        if self.negative_normalization:
+            X = X * 2 - 1
+            
+        data = {"meta_data": X}
         
-        return X, y
+        if self.use_images:
+            imgs = np.load("./train_images.npy", allow_pickle = True)
+            images = np.zeros((self.batch_size, self.dim, self.dim, 3))
+            for i, ID in enumerate(list_IDs_temp):
+                images[i,] = np.asarray(imgs[int(inpdata[i,-1])], dtype = "float32")
+            data["image"] = images
+        
+        if self.predict_slope:
+            data["WeekDiff"] = WeekDiff
+            data["InitFVC"] = InitFVC
+        
+        return data, y
+    
+def make_lungmask(img, display=False):
+    row_size= img.shape[0]
+    col_size = img.shape[1]
+    
+    mean = np.mean(img)
+    std = np.std(img)
+    img = img-mean
+    img = img/std
+    
+    # Find the average pixel value near the lungs
+        # to renormalize washed out images
+    middle = img[int(col_size/5):int(col_size/5*4),int(row_size/5):int(row_size/5*4)] 
+    mean = np.mean(middle)  
+    max = np.max(img)
+    min = np.min(img)
+    
+    # To improve threshold finding, I'm moving the 
+    # underflow and overflow on the pixel spectrum
+    img[img==max]=mean
+    img[img==min]=mean
+    
+    # Using Kmeans to separate foreground (soft tissue / bone) and background (lung/air)
+    
+    kmeans = KMeans(n_clusters=2).fit(np.reshape(middle,[np.prod(middle.shape),1]))
+    centers = sorted(kmeans.cluster_centers_.flatten())
+    threshold = np.mean(centers)
+    thresh_img = np.where(img<threshold,1.0,0.0)  # threshold the image
+
+    # First erode away the finer elements, then dilate to include some of the pixels surrounding the lung.  
+    # We don't want to accidentally clip the lung.
+
+    eroded = morphology.erosion(thresh_img,np.ones([3,3]))
+    dilation = morphology.dilation(eroded,np.ones([8,8]))
+
+    labels = measure.label(dilation) # Different labels are displayed in different colors
+    label_vals = np.unique(labels)
+    regions = measure.regionprops(labels)
+    good_labels = []
+    for prop in regions:
+        B = prop.bbox
+        if B[2]-B[0]<row_size/10*9 and B[3]-B[1]<col_size/10*9 and B[0]>row_size/5 and B[2]<col_size/5*4:
+            good_labels.append(prop.label)
+    mask = np.ndarray([row_size,col_size],dtype=np.int8)
+    mask[:] = 0
+
+
+    #  After just the lungs are left, we do another large dilation
+    #  in order to fill in and out the lung mask 
+    
+    for N in good_labels:
+        mask = mask + np.where(labels==N,1,0)
+    mask = morphology.dilation(mask,np.ones([10,10])) # one last dilation
+
+    return mask*img    
     
 SQRT2 = tf.sqrt(tf.dtypes.cast(2, dtype=tf.float32))
 
@@ -282,26 +363,46 @@ def build_model(config):
     drop_out_layers = config["DROP_OUT_LAYERS"]
     loss_modification = config["LOSS_MODIFICATION"]
     optimal_sigma_loss = config["OPTIMAL_SIGMA_LOSS"]
+    batch_normalization = config["BATCH_NORMALIZATION"]
+    pre_batch_normalization = config["PRE_BATCH_NORMALIZATION"]
+    batch_renormalization = config["BATCH_RENORMALIZATION"]
+    dim = config["DIM"]
+    img_features = config["IMG_FEATURES"]
+    use_imgs = config["USE_IMAGES"]
+    effnet = config["EFFNET"]
     
+    if actfunc == 'relu':
+        actfunc = tf.keras.activations.relu
     if actfunc == 'swish':
         actfunc = tf.keras.activations.swish
     if actfunc == 'leakyrelu':
         actfunc = lambda x: tf.keras.activations.relu(x, alpha=0.3)
 
-    inp = tf.keras.layers.Input(shape=(size), name = "input_features")
-    inp2 = tf.keras.layers.Input(shape=(1), name = "slope_FVC")
-    inp3 = tf.keras.layers.Input(shape=(1), name = "slope_Weekdiff")
+    metadata = tf.keras.layers.Input(shape=(size), name = "meta_data")
+    inputs = [metadata]
     
-    inputs = [inp,inp2,inp3]
-
-    x = inp
+    x = metadata
+    
+    if use_imgs:
+        image = tf.keras.layers.Input(shape=(dim,dim,3), name = "image")
+        inputs.append(image)
+        base = EFNS[effnet](input_shape=(dim,dim,3), weights='imagenet', include_top=False, pooling = 'avg')
+        y = base(image)
+        y = tf.keras.layers.Dense(img_features)(y)
+        y = actfunc(y)
+        x = tf.keras.layers.concatenate([x,y])    
+    
+    if pre_batch_normalization:
+        x = tf.keras.layers.BatchNormalization(renorm = batch_renormalization)(x)
     
     for j,n_neurons in enumerate(hidden_layers):
         if l2_regularization:
-            x = tf.keras.layers.Dense(n_neurons, activation=actfunc,
-                                      kernel_regularizer = tf.keras.regularizers.l2(regularization_constant))(x)
+            x = tf.keras.layers.Dense(n_neurons, kernel_regularizer = tf.keras.regularizers.l2(regularization_constant))(x)
         else:
-            x = tf.keras.layers.Dense(n_neurons, activation=actfunc)(x)
+            x = tf.keras.layers.Dense(n_neurons)(x)
+        if batch_normalization:
+            x = tf.keras.layers.BatchNormalization(renorm = batch_renormalization)(x)
+        x = actfunc(x)
         if j in drop_out_layers:
             x = tf.keras.layers.Dropout(drop_out_rate)(x)
     
@@ -316,8 +417,11 @@ def build_model(config):
             sigma_output = tf.math.scalar_mul(tf.constant(100,dtype = 'float32'), sigma_output)
 
     if predict_slope:
-        FVC_output = tf.add(tf.keras.layers.multiply([FVC_output, inp2]),inp3)
-        sigma_output = tf.keras.layers.multiply([sigma_output, inp2])
+        WeekDiff = tf.keras.layers.Input(shape = (1), name = "WeekDiff")
+        InitFVC = tf.keras.layers.Input(shape = (1), name = "InitFVC")
+        inputs.extend([WeekDiff,InitFVC])
+        FVC_output = tf.add(tf.keras.layers.multiply([FVC_output, WeekDiff]),InitFVC)
+        sigma_output = tf.keras.layers.multiply([sigma_output, WeekDiff])
         
     outputs = tf.keras.layers.concatenate([FVC_output,sigma_output])
 
@@ -331,7 +435,7 @@ def build_model(config):
                       metrics = [Laplace_metric, sigma_cost, delta_over_sigma, absolute_delta_error])
     
     return model
-    
+
 def absolute_delta_error(y_true, y_pred):
 
     FVC_true = y_true[:,0]
